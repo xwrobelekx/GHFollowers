ab1b8698fc920f74e983c53bca165e6061b7ac74
Kamil Wrobel <wrobelk@live.com>
2020-03-04 22:59:46 -0500

    refactors tab bar controller from scene delegate to UITabbar sub class

---
+++ GHFollowers/Custom Views/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,38 @@
+//
+//  GFTabBarController.swift
+//  GHFollowers
+//
+//  Created by Kamil Wrobel on 3/4/20.
+//  Copyright © 2020 Kamil Wrobel. All rights reserved.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers = [createSearchNC(), createFavoritesNC()]
+
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    func createSearchNC() -> UINavigationController {
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesNC() -> UINavigationController {
+        let favoritesVC = FavloritesVC()
+        favoritesVC.title = "Favorites"
+        favoritesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        return UINavigationController(rootViewController: favoritesVC)
+    }
+
+
+}
